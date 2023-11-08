8c63a71a192fe23320e2826c293026a14c46b43f
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2023-11-08 11:49:26 -0500

    Finished v1

--- Multipilcation-Table-Generator/javaScript/index.js
+++ Multipilcation-Table-Generator/javaScript/index.js
@@ -1,81 +1,140 @@
 function createTable() {
+
     // clear any error messeages
     // clear table
     clearTable();
     clearError();
-    // minRow, maxRow, minCol, maxCol
+
     // get rows and columns from input and check if valid
-    var minRow = document.getElementById('min-row').value;
-    console.log("MinRow: " + parseInt(minRow,10));
-    if (parseInt(minRow,10) === "") {
-        errorMessage("Invalid input for minimum row.");
-        clearInput();
-        return;
-    }
-    var maxRow = document.getElementById('max-row').value;
-    console.log("MaxRow: " + maxRow);
-    if(parseInt(maxRow, 10) < parseInt(minRow, 10)|| parseInt(maxRow,10) === "") {
-        errorMessage("Invalid input for max row.");
-        clearInput();
-        return;
-    }
-    var minCol = document.getElementById('min-col').value;
-    if (parseInt(minCol,10) === "") {
-        errorMessage("Invalid input for minimum column.");
-        clearInput();
-        return;
-    }
-    var maxCol = document.getElementById('max-col').value;
-    if(parseInt(maxCol, 10) < parseInt(minCol, 10) || parseInt(maxCol,10) === "") {
-        errorMessage("Invalid input for max column.");
-        clearInput();
-        return;
-    }
+    var minRow = document.getElementById('min-row');
+    minRow.style.backgroundColor = "#fff";
+    var maxCol = document.getElementById('max-col');
+    maxCol.style.backgroundColor = "#fff";
+    var maxRow = document.getElementById('max-row');
+    maxRow.style.backgroundColor = "#fff";
+    var minCol = document.getElementById('min-col');
+    minCol.style.backgroundColor = "#fff";
     
+
+    
+
+    if(checkInput(minRow, maxRow, minCol, maxCol) > 0) {
+        clearInput();
+        return;
+    }
     // seperate row an column amounts from the values for the table
-
-    var totalRows = maxRow - minRow;
-    var totalCols = maxCol - minCol;
+    var totalRows = (maxRow.value) - (minRow.value);
+    var totalCols = (maxCol.value) - (minCol.value);
     console.log(totalCols);
-    //create first row and column
 
+    //create first row and column
     var table; 
     var cell;
-    // fill the rows with cells
-    for(var i = 0; i <= totalRows + 1; i++){
-        table = document.getElementById("mult-table").insertRow(i);
-        for(var j = 0; j <= totalCols + 1; j++) {
-            // (0,0) location
-            cell = table.insertCell(j);
-            if(i === 0 && j === 0) {
-                cell.innerHTML = " ";
-                cell.classList.add("null-cell");
-            }
-            // (n,0) row
-            else if (i === 0 && j > 0) {
-                cell.innerHTML = parseInt(j, 10) + parseInt(minCol, 10) - 1;
-                cell.classList.add("cell");
-                
-            }
-            // (0, n) column
-            else if (j === 0 && i > 0){ 
+    var row;
 
-                cell.innerHTML = parseInt(i, 10) + parseInt(minRow,10) - 1;
-                cell.classList.add("cell");
-            } 
+    table = document.getElementById("mult-table");
+    var thead;
+
+    thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    var headerRow = document.createElement("tr");
+    table.appendChild(headerRow);
+
+    // fill top of table
+    for (var i = 0; i <= totalCols + 1; i++) {
+        var th = document.createElement("th");
+        if (i > 0) {
+            th.textContent = (parseInt(i, 10) + parseInt(minRow.value,10) - 1);
+            th.classList.add("cell");
+            th.style.position = "sticky";
+            th.style.top = "0";
+            headerRow.appendChild(th);
+        } else {
+            th.textContent = " ";
+            headerRow.appendChild(th);
+        }
+    }
+
+    var tableBody = document.createElement("tbody");
+    table.appendChild(tableBody);
+    // fill the rows with cells
+    for(var i = 1; i <= totalRows+1; i++){
+        row = document.createElement("tr");
+        tableBody.appendChild(row);
+        th = document.createElement("th");
+        th.textContent = parseInt(i, 10) + parseInt(minCol.value, 10) - 1;
+        th.classList.add("cell");
+        th.style.position = "sticky";
+        th.style.left = "0";
+        row.appendChild(th);
+        
+        for(var j = 1; j <= totalCols+1; j++) {
+            console.log("Row: " + i + "Cell: " + j);
+            // (0,0) location
+            cell = document.createElement("td");
+            // (n,0) row  
             // everything else, perfrom multiplcation, and style based on even or odd values
-            else {
-                var value = (parseInt(j,10) + parseInt(minCol, 10) - 1) * (parseInt(minRow, 10) + parseInt(i, 10) - 1);
-                cell.innerHTML = value;
-                if (j % 2 === 0){
-                    cell.classList.add("evenCell");
-                } else {
-                    cell.classList.add("oddCell");
-                }
-            }   
+            var value = (parseInt(j,10) + parseInt(minCol.value, 10) - 1) * (parseInt(minRow.value, 10) + parseInt(i, 10) - 1);
+            cell.textContent = (value);
+            if (j % 2 === 0){
+                cell.classList.add("evenCell");
+            } else {
+                cell.classList.add("oddCell");
+            }
+            row.appendChild(cell);
+             
         }
     }
     clearInput();
+}
+
+function checkInput(minRow, maxRow, minCol, maxCol) {
+    var inputErrors = 0;
+    
+    if (minRow.value === "") {
+        errorMessage("Invalid input for minimum row.\n", minRow);
+        inputErrors++;
+    }
+    
+    if (parseInt(minRow.value, 10) > 200 || parseInt(minRow.value, 10) < -200) {
+        errorMessage("Invalid: min row can't be greater than 200 or less than -200", minRow);
+        inputErrors++;
+    } 
+
+    console.log("MaxRow: " + maxRow.value);
+    if(maxRow.value === "" || parseInt(maxRow.value, 10) < parseInt(minRow.value, 10) ) {
+        errorMessage("Invalid input for max row.", maxRow);
+        inputErrors++;
+    }
+    
+    if (parseInt(maxRow.value,10) > 200 || parseInt(maxRow.value,10) < -200) {
+        errorMessage("Invalid: max row can't be greater than 200 or less than -200", maxRow);
+        inputErrors++;
+    }
+
+    console.log("MinRow: " + parseInt(minRow,10));
+    if (minCol.value === "") {
+        errorMessage("Invalid input for minimum column.\n", minCol);
+        inputErrors++;
+    }
+    
+    if (parseInt(minCol.value, 10) > 200 || parseInt(minCol.value, 10) < -200) {
+        errorMessage("Invalid: min column can't be greater than 200 or less than -200", minCol);
+        inputErrors++;
+    } 
+
+    if(maxCol.value === "" || parseInt(maxCol.value, 10) < parseInt(minCol.value, 10)) {
+        errorMessage("Invalid input for max column.\n", maxCol);
+        inputErrors++;
+    }
+    
+    if (parseInt(maxCol.value,10) > 200 || parseInt(maxCol.value,10) < -200) {
+        errorMessage("Invalid: max column can't be greater than 200 or less than -200", maxCol);
+        inputErrors++;
+    }
+
+    return inputErrors;
 }
 
 function clearTable() {
@@ -85,6 +144,7 @@ function clearTable() {
 }
 
 function clearError() {
+    
     // remove everything associated with invalid-Input
     var error = document.getElementById("invalid-Input");
     if(error.children.length > 0) {
@@ -107,7 +167,7 @@ function clearInput() {
     item.value = "";
 }
 
-function errorMessage(error) {
+function errorMessage(error, inputBox) {
     // given a messaage generate an error message under the invalid-Input
     const para = document.createElement("p");
     const node = document.createTextNode(error);
@@ -115,6 +175,9 @@ function errorMessage(error) {
     const element = document.getElementById("invalid-Input");
     element.appendChild(para); 
     element.classList.add("error-msg");
+
+    // highlight input box
+    inputBox.style.backgroundColor = "yellow";
 }
 
 // give callback method and event to the listener
